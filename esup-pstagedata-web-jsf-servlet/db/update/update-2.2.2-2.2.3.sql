9f7234d4da85e55d9c34e050170f6605cc3e05d8
fgarot <florian.garot@univ-artois.fr>
2015-02-12 13:53:57 +0100

    - Mise à jour du numéro de version - Initialisation par défaut à 4 (= 'mois') pour le nouveau champs idUniteDureeGratification.

--- esup-pstagedata-web-jsf-servlet/db/update/update-2.2.2-2.2.3.sql
+++ esup-pstagedata-web-jsf-servlet/db/update/update-2.2.2-2.2.3.sql
@@ -4,6 +4,6 @@ ALTER TABLE Structure CHANGE estValidee estValidee INT NOT NULL DEFAULT 0;
 UPDATE UniteDuree SET temEnServUniteDuree='N' WHERE idUniteDuree=2 OR idUniteDuree=3 OR idUniteDuree=5;
 UPDATE UniteDuree SET libelleUniteDuree='heure' WHERE idUniteDuree=1;
 
-ALTER TABLE Convention ADD idUniteDureeGratification INT NULL;
+ALTER TABLE Convention ADD idUniteDureeGratification INT NULL DEFAULT 4;
 
-ALTER TABLE Convention ADD monnaieGratification VARCHAR(50) NULL DEFAULT 'euros'
+ALTER TABLE Convention ADD monnaieGratification VARCHAR(50) NULL DEFAULT 'euros';
