d3ffe6e0b46bc979fe6ee9d2c5a9f4d696781056
fgarot <florian.garot@univ-artois.fr>
2015-07-13 13:31:41 +0200

    - script de mise à jour de la BDD : ajout du champs commentaireRupture et correction de l'ajout du temoinEnServStructure

--- esup-pstagedata-web-jsf-servlet/db/update/update-2.2.3-2.2.4.sql
+++ esup-pstagedata-web-jsf-servlet/db/update/update-2.2.3-2.2.4.sql
@@ -1,6 +1,8 @@
 
 ALTER TABLE Avenant ADD dateRupture DATE NULL;
 
-ALTER TABLE Structure ADD temEnServStructure VARCHAR(1)  'O';
+ALTER TABLE Avenant ADD commentaireRupture TEXT NULL;
 
-ALTER TABLE Convention CHANGE nbJoursHebdo nbJoursHebdo ENUM('0.5','1','1.5','2','2.5','3','3.5','4','4.5','5') NOT NULL DEFAULT '5';
+ALTER TABLE Structure ADD temEnServStructure VARCHAR(1) DEFAULT 'O';
+
+ALTER TABLE Convention CHANGE nbJoursHebdo nbJoursHebdo ENUM('0.5','1','1.5','2','2.5','3','3.5','4','4.5','5','5.5','6') NOT NULL DEFAULT '5';
