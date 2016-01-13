88699498dfdd1b16169259b4e8916282175f5615
fgarot <florian.garot@univ-artois.fr>
2016-01-13 14:35:18 +0100

    Ajout via "Add to git index" d'un fichier ignoré par le commit précédent

---
+++ esup-pstagedata-web-jsf-servlet/db/update/update-2.2.5-2.2.6.sql
@@ -0,0 +1,21 @@
+
+-- Augmentation de la taille du champs campus qui peut etre plus grand que 50 caractere dans certains cas
+ALTER TABLE Enseignant CHANGE campus campus VARCHAR(250) NULL;
+
+-- Passage du codeCommune de la table offre en varchar pour prendre en compte les codes de la corse
+ALTER TABLE Offre CHANGE codeCommune codeCommune VARCHAR(10) NULL;
+
+-- Recreation de l'index sur la table Etape car il semblerait qu'il ne soit pas bon pour certaines universités et entraine des erreurs lors de la creation de convention
+ALTER TABLE Etape DROP INDEX index_codeEtape_codeUniversite;
+
+CREATE UNIQUE INDEX index_codeEtape_codeUniversite ON Etape (codeEtape ASC, codeVersionEtape ASC, codeUniversite ASC) ;
+
+INSERT INTO LangueConvention (
+   codeLangueConvention
+  ,libelleLangueConvention
+  ,temEnServLangue
+) VALUES (
+   'it'  -- codeLangueConvention
+  ,'Italien (Convention, stage étranger)'  -- libelleLangueConvention
+  ,'O'  -- temEnServLangue
+);
